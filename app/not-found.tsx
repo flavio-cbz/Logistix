0fb93fb34b75f89f1daf30d84abdca084a5111ec
Flavio <33604967+flavio-cbz@users.noreply.github.com>
2025-03-26 00:06:59 +0100

    Delete app/not-found.tsx

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <main style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Page non trouvée</h1>
-      <p>Désolé, la page que vous recherchez n'existe pas.</p>
-    </main>
-  );
-}
